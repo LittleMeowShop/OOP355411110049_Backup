9cad634df9586d8d16eca6370841eb71e752cb5b
Little Cat <nekozawa_vamprie@hotmail.com>
2018-11-26 14:23:20 +0700

    Initial commit

---
+++ src/Lab2/Ex1.java
@@ -0,0 +1,32 @@
+package Lab2;
+
+public class Ex1 {
+    //integer
+    public static void main(String[] args) {
+        int a;
+        a=10;
+        System.out.println(a);
+        int b=20;
+        System.out.println(b);
+        int c=a+b;
+        System.out.println("ผลรวม a และ b เท่ากับ\t"+c);
+
+        //real number
+        double f;
+        f=5.5;
+        System.out.println(f);
+        f=f*10;
+        System.out.println(f);
+
+        //character
+        char d = '#';
+        System.out.println(d);
+
+        //String
+        String str="Tannalyn Tassankowit";
+        System.out.println("My name is\t"+str);
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+
+    }//main
+}//class
