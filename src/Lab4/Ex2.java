3224f666e4a84c0b27f24271f5f4b775710fe9cc
Little Cat <nekozawa_vamprie@hotmail.com>
2018-12-17 14:07:12 +0700

    Lab4

--- src/Lab4/Ex2.java
+++ src/Lab4/Ex2.java
@@ -2,6 +2,10 @@ package Lab4;
 
 public class Ex2 {
     public static void main(String[] args) {
-        int x = sum(10, 10);
+        int x = summation(10, 10);
+    }
+
+    private static int summation(int i, int i1) {
+        return i + i1;
     }
 }
